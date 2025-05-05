67b24e55ad94e31216690c04f46d57397e3b2056
benjamin-abebe <benjamin.abebe@stu.ocsb.ca>
2025-05-05 23:34:06 +0000

    innit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
-// 
+//
 // Created by: Benjamin Abebe
 // Created on: Feb 2025
 // This file contains the JS functions for index.html
@@ -10,10 +10,12 @@ function myButtonClicked() {
   let sizePrice = 0
   let size = ""
   let team = ""
+  let jerseyPrice = 0
+  let totalPrice = 0
 
   // Get the selected size by the user
   if (document.getElementById("size-s").checked) {
-    sizePrice = 20
+    sizePrice = 30
     size = "Small"
   } else if (document.getElementById("size-m").checked) {
     sizePrice = 40
@@ -23,29 +25,31 @@ function myButtonClicked() {
     size = "Large"
   } else if (document.getElementById("size-xl").checked) {
     sizePrice = 70
-    size = "Extra-large"
-  } else{
-    // You should not be here!
+    size = "Extra Large"
   }
 
   // Get selected team by the user
   if (document.getElementById("team-barca").checked) {
     team = "Barcelona"
+    jerseyPrice = 20
   } else if (document.getElementById("team-madrid").checked) {
     team = "Real Madrid"
+    jerseyPrice = 5
   } else if (document.getElementById("team-liverpool").checked) {
     team = "Liverpool"
+    jerseyPrice = 30
   } else if (document.getElementById("team-arsenal").checked) {
     team = "Arsenal"
-  } else {
-    // You should not be here!
+    jerseyPrice = 10
   }
 
-  // calculte the whole process of the size and the teams jersery that are selected
+  // Calculate the final result
   if (size && team) {
-    sizePrice = sizePrice.toFixed(2)
-    document.getElementById("answer").innerHTML = "<p>You ordered a " + size + " size " + team + " jersey for $" + sizePrice + "</p>"
+    totalPrice = "$" + (sizePrice + jerseyPrice).toFixed(2)
+    document.getElementById("answer").innerHTML = 
+      "<p>You ordered a " + size + " size " + team + " jersey for " + totalPrice + "</p>"
   } else {
-    document.getElementById("answer").innerHTML = "<p>Please make sure to select both a size and a team to calculate and order.</p>"
+    document.getElementById("answer").innerHTML = 
+      "<p>Please make sure to select both a size and a team to calculate and order.</p>"
   }
 }
